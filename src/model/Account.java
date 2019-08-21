feca407f0edaacb0f4e34967273d91b8882dacdd
Marco <Marco@DESKTOP-U44C2UT>
2019-08-21 23:07:18 +0200

    From AUTO to IDENTITY

--- src/model/Account.java
+++ src/model/Account.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 public class Account {
 	
 	@Id
-	@GeneratedValue (strategy = GenerationType.AUTO)
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
 	private int id;
 	
 	@Column(unique = true)
