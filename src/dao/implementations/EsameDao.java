b7994a0a21a7dbbef1b5db713e1daaba01093794
Marco <Marco@DESKTOP-U44C2UT.station>
2019-08-22 14:35:44 +0200

    EsameInterface + EsameDao

---
+++ src/dao/implementations/EsameDao.java
@@ -0,0 +1,93 @@
+package dao.implementations;
+
+import java.util.ArrayList;
+
+import org.hibernate.Session;
+
+import dao.interfaces.EsameInterface;
+import model.Esame;
+import model.Facolta;
+import util.HibernateUtil;
+
+public class EsameDao implements EsameInterface{
+	public void inserimento(Esame e){
+		Session session = HibernateUtil.getSessionFactory().openSession();
+
+		try{
+			session.beginTransaction();
+
+			session.save(e);
+
+			session.getTransaction().commit();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			session.getTransaction().rollback();
+		} finally{
+			session.close();
+		}
+	}
+	
+	@Override
+	public Esame getById(int id){
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		Esame esame;
+		
+		try{
+			session.beginTransaction();
+						
+			esame = (Esame) session.get(Facolta.class, id);
+						
+			session.getTransaction().commit();
+
+			return esame;
+		} catch (Exception e) {
+			System.out.println("Error in getAll()");
+			e.printStackTrace();
+			return null;
+		} finally {
+			session.close();
+		}
+	}
+	@SuppressWarnings("unchecked")
+	@Override
+	public ArrayList<Esame> getAll() {
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		ArrayList<Esame> esami;
+		
+		try{
+			session.beginTransaction();
+
+			esami = (ArrayList<Esame>) session.createQuery("from Esame").list();
+			
+			session.getTransaction().commit();
+
+			return esami;
+		} catch (Exception e) {
+			System.out.println("Error in getAll()");
+			return null;
+		} finally {
+			session.close();
+		}
+	}
+	@Override
+	public void update(Esame e) {
+		Session session = HibernateUtil.getSessionFactory().openSession();
+
+		try{
+			session.beginTransaction();
+
+			session.update(e);
+
+			session.getTransaction().commit();
+		} catch (Exception ex) {
+			session.getTransaction().rollback();
+		} finally{
+			session.close();
+		}		
+	}
+
+	@Override
+	public void remove(Esame element) {
+		
+	}
+}
